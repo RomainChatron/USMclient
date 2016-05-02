78d108cd98c3a993a9b7affde52046639c6d4e04
Leah <Leah@wificampus-028166.grenet.fr>
2016-05-02 17:15:36 +0200

    delete Group via admin

--- USMclient/src/appTemoin1/DeleteGroupe.java
+++ USMclient/src/appTemoin1/DeleteGroupe.java
@@ -6,9 +6,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import vInterface._Group;
+import vInterface._User;
+import vInterfaceDB._UserDB;
+import vInterfaceDB._GroupDB;
+
 import java.awt.Toolkit;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
@@ -17,6 +25,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DeleteGroupe extends JFrame {
@@ -64,16 +74,45 @@ public class DeleteGroupe extends JFrame {
 		label.setBounds(29, 29, 103, 48);
 		contentPane.add(label);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(268, 103, 36, 26);
-		contentPane.add(comboBox);
+		try {
+			ArrayList<_Group> g =  ((_GroupDB) rmi.get("GroupDB")).getAllGroups();
+			String[] allGroups = new String[g.size()+1];
+			allGroups[0] = "Choisissez le groupe..";
+			for (int i=1; i<g.size()+1;i++){
+				allGroups[i] = g.get(i-1).getName();
+			}
+			
+			JComboBox cbGroups = new JComboBox(allGroups);
+			cbGroups.setBounds(61, 118, 281, 26);
+			contentPane.add(cbGroups);
+			
 		
-		JButton btnNewButton = new JButton("Supprimer");
-		btnNewButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
-		btnNewButton.setBackground(Color.BLACK);
-		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setBounds(59, 170, 115, 29);
-		contentPane.add(btnNewButton);
+		JButton btnSupprimer = new JButton("Supprimer");
+		btnSupprimer.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					((_GroupDB) rmi.get("GroupDB")).removeGroup(g.get(cbGroups.getSelectedIndex()-1).getIdG());
+					JOptionPane.showMessageDialog(null, "Le groupe a bien \u00E9t\u00E9 supprim\u00E9 !");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		
+		
+		
+		btnSupprimer.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
+		btnSupprimer.setBackground(Color.BLACK);
+		btnSupprimer.setForeground(Color.WHITE);
+		btnSupprimer.setBounds(59, 170, 115, 29);
+		contentPane.add(btnSupprimer);
+		
+		} catch (RemoteException e1) {
+			e1.printStackTrace();
+		}
+		
 		
 		JButton btnNewButton_1 = new JButton("Annuler");
 		btnNewButton_1.addMouseListener(new MouseAdapter() {
