9c0278d9a14cd2045d4ded3015a1bce2874e7bba
Leah <Leah@wificampus-028166.grenet.fr>
2016-05-02 15:59:25 +0200

    deleteUser

--- USMclient/src/appTemoin1/DeleteUser.java
+++ USMclient/src/appTemoin1/DeleteUser.java
@@ -6,15 +6,26 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import vInterface._ObjectBuilder;
+import vInterface._User;
+import vInterfaceDB._UserDB;
+
 import java.awt.Toolkit;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import java.awt.Font;
 import javax.swing.JButton;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DeleteUser extends JFrame {
@@ -63,16 +74,43 @@ public class DeleteUser extends JFrame {
 		lblNewLabel.setBounds(15, 124, 192, 20);
 		contentPane.add(lblNewLabel);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(209, 122, 36, 26);
-		contentPane.add(comboBox);
+		try {
+			ArrayList<_User> u =  ((_UserDB) rmi.get("UserDB")).allUsers();
+			String[] allUsers = new String[u.size()+1];
+			allUsers[0] = "Choisissez l'utilisateur..";
+			for (int i=1; i<u.size()+1;i++){
+				allUsers[i] = u.get(i).getUserName()+" : "+u.get(i).getFirstName()+" "+u.get(i).getLastName();
+			}
+			
+			
+			JComboBox cbUsers = new JComboBox(allUsers);
+			cbUsers.setBounds(209, 122, 36, 26);
+			contentPane.add(cbUsers);
+			
 		
-		JButton btnNewButton = new JButton("Supprimer");
-		btnNewButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
-		btnNewButton.setBackground(Color.BLACK);
-		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setBounds(63, 174, 115, 29);
-		contentPane.add(btnNewButton);
+		JButton btnSupprimer = new JButton("Supprimer");
+		btnSupprimer.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					((_UserDB) rmi.get("UserDB")).removeUser(u.get(cbUsers.getSelectedIndex()-1).getUserName());
+					JOptionPane.showMessageDialog(null, "L'utilisateur a bien \u00E9t\u00E9 supprim\u00E9 !");
+				} catch (RemoteException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		btnSupprimer.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
+		btnSupprimer.setBackground(Color.BLACK);
+		btnSupprimer.setForeground(Color.WHITE);
+		btnSupprimer.setBounds(63, 174, 115, 29);
+		contentPane.add(btnSupprimer);
+		
+		} catch (RemoteException e1) {
+			e1.printStackTrace();
+		}
 		
 		JButton btnAnnuler = new JButton("Annuler");
 		btnAnnuler.addMouseListener(new MouseAdapter() {
